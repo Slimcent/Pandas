d0f2ec8f0cb3c7584483ced9e2966ab05cc3a037
Slimcent <centpresley@gmail.com>
2023-08-07 19:47:10 +0100

    Pandas DataFrame

--- main.py
+++ main.py
@@ -21,4 +21,22 @@ print(mySeries)
 
 print()
 
+#  DataFrames, this is a whole table of rows and columns
+myDataset = {
+    'cars': ["BMW", "Volvo", "Ford"],
+    'passings': [3, 7, 2]
+}
+myVar = pd.DataFrame(myDataset)
+print(myVar)
 
+print(myVar.loc[0])  # loc is used to print a row, here we are printing the first row
+print(myVar.loc[[0, 1]])  # here we are printing the first and second row
+
+data = {
+    "calories": [420, 380, 390],
+    "duration": [50, 40, 45]
+}
+df = pd.DataFrame(data, index=["day1", "day2", "day3"])  # Using names to hold the indexes
+print(df)
+
+print(df.loc["day2"])  # Locating a row using a named index
